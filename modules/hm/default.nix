28f0ac4f07acf021d7a22c42b1f53189e9ebf8ad
santamn <cle.neige@gmail.com>
2026-02-11 19:53:57 +0900

    右クリックの設定完成

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -90,7 +90,6 @@
             # スクロール速度を半分にする
             scroll_factor = 0.5
             # 2本指での物理クリック（押し込み）を右クリックとして扱う
-            # clickfinger_behavior = true  # tap_button_map と競合するため無効化
             clickfinger_behavior = true
             # 2本指タップを中クリック扱いにし、右クリックメニューの誤爆を防ぐ
             tap_button_map = lmr
