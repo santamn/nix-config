6307dac4cff0f8012a97bef5fe27c9410b3f7afd
santamn <cle.neige@gmail.com>
2026-02-13 22:10:44 +0900

    ヤンクしたテキストをクリップボードにも保存

--- modules/hm/programs/neovim.nix
+++ modules/hm/programs/neovim.nix
@@ -1,8 +1,14 @@
 {
   config,
+  pkgs,
   nixConfigPath,
   ...
 }: {
+  # Linux環境でのクリップボード共有のためのツール
+  home.packages = with pkgs; [
+    wl-clipboard # Wayland用
+  ];
+
   # ~/.config/nvim を ~/dotnix/nvim への直リンクに: どちらのファイルを編集しても即座に反映さる
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/nvim";
 }
--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -43,6 +43,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        clipboard = "unnamedplus", -- sync clipboard with OS
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
