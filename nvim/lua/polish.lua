ce009e490db0d10674c45f9581a5a9a4adf0144a
santamn <cle.neige@gmail.com>
2026-02-14 14:37:26 +0900

    中クリックのペーストを無効化

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,10 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- 中クリックペーストを無効化
+-- ノーマル(n), 挿入(i), ビジュアル(v), コマンドライン(c) モードを対象
+vim.keymap.set({ "n", "i", "v", "c" }, "<MiddleMouse>", "<Nop>", { silent = true })
+vim.keymap.set({ "n", "i", "v", "c" }, "<2-MiddleMouse>", "<Nop>", { silent = true })
+vim.keymap.set({ "n", "i", "v", "c" }, "<3-MiddleMouse>", "<Nop>", { silent = true })
+vim.keymap.set({ "n", "i", "v", "c" }, "<4-MiddleMouse>", "<Nop>", { silent = true })
