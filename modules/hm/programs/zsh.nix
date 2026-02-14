ec670dfabb571e8c914f0e4817fe2122d9148eae
santamn <cle.neige@gmail.com>
2026-02-14 11:46:46 +0900

    fix nushell starups

--- modules/hm/programs/zsh.nix
+++ modules/hm/programs/zsh.nix
@@ -1,12 +1,16 @@
-{...}: {
+{lib, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
 
     # インタラクティブセッションでnushellを起動
-    initContent = ''
-      # インタラクティブシェルの場合のみnushellを起動
-      if [[ $- == *i* ]] && [[ -z "$NUSHELL_ACTIVE" ]]; then
+    initContent = lib.mkBefore ''
+      # インタラクティブシェルの場合のみ nushell を起動
+      # Neovim のターミナルでは、親シェルから NUSHELL_ACTIVE が引き継がれて
+      # zsh->nu への自動切り替えが抑止されることがあるため、NVIM 配下では常に許可する
+      if [[ $- == *i* ]] \
+        && command -v nu >/dev/null 2>&1 \
+        && { [[ -z "$NUSHELL_ACTIVE" ]] || [[ -n "$NVIM" ]]; }; then
         export NUSHELL_ACTIVE=1
         exec nu
       fi
